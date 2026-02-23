f77c62382411a6b389e5880f3f09f72eb7703f68
Nathan Barry <nathanbarry474@gmail.com>
2026-02-23 03:31:30 -0600

    add fallback to hf imports if jax dep unavailable

--- gidd_easydel/model/__init__.py
+++ gidd_easydel/model/__init__.py
@@ -1,11 +1,16 @@
-from .gidd_configuration import GiddConfig
-from .modeling_gidd import (
-    GiddModel,
-    GiddForDiffusionLM,
-)
+try:
+    from .gidd_configuration import GiddConfig
+    from .modeling_gidd import (
+        GiddModel,
+        GiddForDiffusionLM,
+    )
+except ImportError:
+    from .configuration_gidd import GiddConfig
+    from .modeling_gidd_hf import (
+        GiddForDiffusionLM,
+    )
 
 __all__ = (
     "GiddConfig",
-    "GiddModel",
     "GiddForDiffusionLM",
 )
